f4a11b0baed087e253303d0207197735befbb8dd
Colin Kennedy <colinvfx@gmail.com>
2018-11-18 00:05:49 -0500

    Make a copy of 'rezzurect' and added it to 'respawn_includes'. 'respawn_includes' will now fall-back to its local copy of 'rezzurect' if REZZURECT_LOCATION is not defined

--- respawn_includes/1.0.0/package.py
+++ respawn_includes/1.0.0/package.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-#
+# -*- coding: utf-8 -*-
 
 name = 'respawn_includes'
 
@@ -14,10 +13,23 @@ build_command = "python {root}/rezbuild.py {install}"
 
 def commands():
     # IMPORT STANDARD LIBRARIES
+    import inspect
     import sys
     import os
 
-    rezzurect_location = os.environ['REZZURECT_LOCATION']
+    # Ideally, this should always be set but it's not required
+    rezzurect_location = os.getenv('REZZURECT_LOCATION', '')
+
+    if not os.path.isdir(rezzurect_location):
+        # From some reason, a Rez package.py doesn't let you do `os.path.realpath(__file__)
+        # so we need to use the stack frame
+        # And the stack frame returns as "</path/to/package.py:commands>"
+        # so we need to strip parts of the returned string
+        #
+        package_path = inspect.getframeinfo(inspect.currentframe()).filename
+        package_path = package_path[1:-1 * len(':commands>')]
+        rezzurect_location = os.path.join(os.path.dirname(package_path), 'python')
+
     sys.path.append(rezzurect_location)
     env.PYTHONPATH.append(rezzurect_location)
 
--- respawn_includes/1.0.0/rezbuild.py
+++ respawn_includes/1.0.0/rezbuild.py
@@ -2,12 +2,48 @@
 # -*- coding: utf-8 -*-
 
 # IMPORT STANDARD LIBRARIES
+import shutil
 import sys
 import os
 
 
 def build(source_path, build_path, install_path, targets):
-    pass
+    '''Create a local copy of `rezzurect` into this Rez package.
+
+    This copy will be used if the user's `REZZURECT_LOCATION` is not defined.
+
+    Args:
+        source_path (str):
+            The absolute path to where this Rez package exists on-disk.
+        build_path (str):
+            The absolute path to the folder where this package's
+            "build.rxt" file will be generated. Usually, this is a subdirectory
+            inside of `source_path`.
+        install_path (str):
+            The absolute path of where `rezzurect` will be copied to.
+        targets (list[str]):
+            Optional arguments that are given by the user from the command-line.
+
+    '''
+    try:
+        import rezzurect
+    except ImportError:
+        # Ideally, this should always be set but it's not required
+        rezzurect_location = os.path.join(os.getenv('REZZURECT_LOCATION', ''), 'rezzurect')
+    else:
+        rezzurect_location = os.path.dirname(rezzurect.__file__)
+
+    if not rezzurect_location or not os.path.isdir(rezzurect_location):
+        return
+
+    root_destination = os.path.join(install_path, 'python')
+
+    if os.path.isdir(root_destination):
+        shutil.rmtree(root_destination)
+        os.makedirs(root_destination)
+
+    destination = os.path.join(root_destination, os.path.basename(rezzurect_location))
+    shutil.copytree(rezzurect_location, destination)
 
 
 if __name__ == '__main__':
