c98ee03ab1f4c75474260000a523ffaf9cd13e46
Colin Kennedy <colinvfx@gmail.com>
2018-12-03 01:11:27 -0500

    tank/1.0.0/package.py - Changed the command which finds the user's path to 'tank' 'sgtk' and 'tank_vendor' to work in Windows

--- tank/1.0.0/package.py
+++ tank/1.0.0/package.py
@@ -13,4 +13,23 @@ build_command = "python {root}/rezbuild.py {install}"
 
 def commands():
     '''Create the environment variables and aliases needed to run this product.'''
-    env.PYTHONPATH.append('/home/selecaoone/.shotgun/shenanigans/p87c36.basic.desktop/cfg/install/core/python')
+    import platform
+    import glob
+    import os
+
+    if platform.system() == 'Windows':
+        _ROOT = os.path.join(os.path.expanduser('~'), 'AppData', 'Roaming', 'Shotgun')
+    elif platform.system() == 'Linux':
+        _ROOT = os.path.join(os.path.expanduser('~'), '.shotgun')
+    else:
+        raise NotImplementedError('Need the mac root folder, here')
+
+    env.PYTHONPATH.append(
+        sorted(glob.glob(os.path.join(
+         _ROOT,
+         'bundle_cache',
+         'app_store',
+         'tk-core',
+         'v*',
+         'python')))[-1]
+    )
